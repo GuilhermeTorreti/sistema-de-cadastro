a444613559408cfaaf037d86fd4ccda9171352d1
GuilhermeTorreti <guitorreti@hotmail.com>
2022-10-18 09:55:12 -0300

    Adicionando um console log

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
+console.log("Aplicação esta se conectando ao banco de dados")
+//mais um console log de teste
+console.log("O Guilherme fez esse commit sem voce ver!")
+console.log("Enviando para o GitHub")
